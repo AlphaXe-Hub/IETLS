1d892fe5e886fb0c52780655188f15302daf83db
xuchenzhe <xuchenzhe@reda-flight.com>
2024-11-27 14:24:30 +0800

    package

--- IETLS.py
+++ IETLS.py
@@ -1,4 +1,3 @@
-# We import WindowsToaster and a toast format we want
 from windows_toasts import WindowsToaster, Toast
 import time
 
@@ -8,16 +7,22 @@ def notification(word):
     # Initialise the toast
     newToast = Toast()
     # Set the body of the notification
-    # for i in (0,10):
     newToast.text_fields = word
     # And display it!
     toaster.show_toast(newToast)
-
-with open('IELTS.txt', 'r',encoding='utf-8') as file: 
-    lines = file.readlines() 
-    for line in lines: 
-        print(line)
-        notification(list(filter(None,line.split('%'))))
-        time.sleep(60)
-
+def main():
+    print("雅思乱序表 启动    tip：在下方输入时间间隔按回车开始。     若想关闭直接按ctrl+c 或者关闭该命令框")
+    second = int(input("设定间隔时常(单位秒): "))
+    with open('IETLS.txt', 'r',encoding='utf-8') as file: 
+        lines = file.readlines() 
+        for line in lines: 
+            print(line)
+            notification(list(filter(None,line.split('%'))))
+            time.sleep(second)
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception as e:
+        print('error: ',e)
+        input("按任意键继续")
 
