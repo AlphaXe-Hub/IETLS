c29ca50854696a4a639c6d61adea5f3eb7438f77
xuchenzhe <xuchenzhe@reda-flight.com>
2024-11-28 10:03:56 +0800

    新增乱序功能

--- IETLS.py
+++ IETLS.py
@@ -1,5 +1,7 @@
 from windows_toasts import WindowsToaster, Toast
 import time
+import random
+import linecache
 
 def notification(word):
     # Prepare the toaster for bread (or your notification)
@@ -11,14 +13,28 @@ def notification(word):
     # And display it!
     toaster.show_toast(newToast)
 def main():
-    print("雅思乱序表 启动    tip：在下方输入时间间隔按回车开始。     若想关闭直接按ctrl+c 或者关闭该命令框")
+    print("雅思单词表表 启动    tip：在下方输入时间间隔按回车开始。     若想关闭直接按ctrl+c 或者关闭该命令框")
     second = int(input("设定间隔时常(单位秒): "))
-    with open('word.txt', 'r',encoding='utf-8') as file: 
+    fileName = 'word.txt'
+    with open(fileName, 'r',encoding='utf-8') as file: 
         lines = file.readlines() 
-        for line in lines: 
-            print(line)
-            notification(list(filter(None,line.split('%'))))
-            time.sleep(second)
+        method = int(input("设定顺序 1正序 2乱序"))
+        if method == 1:
+            for line in lines: 
+                print(line)
+                # notification(list(filter(None,line.split('%'))))
+                time.sleep(second)
+        else:
+            count = len(lines)
+            while True:
+                index = random.randint(0,count)
+                line = linecache.getline(fileName,index).strip()
+                print(line)
+                notification(list(filter(None,line.split('%'))))
+                time.sleep(second)
+
+
+
 if __name__ == "__main__":
     try:
         main()
